91e3af7a55d381bac1124204a82d8bbcb2889cd4
CL-Palak <Palak.chauhan@cirruslabs.io>
2026-01-21 10:43:54 +0530

    fix: added tests folder

--- scrum_master_2-latest/main.py
+++ scrum_master_2-latest/main.py
@@ -1,7 +1,9 @@
 
 
 import uvicorn
-from src.api.app import app
+from app.db import db  # example
+from safe_api import app
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
